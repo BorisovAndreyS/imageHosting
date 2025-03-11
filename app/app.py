41b66a26f401d6673f6e217217eba26b1a8c68b6
helmut <helmut.andrey@gmail.com>
2025-03-11 12:38:39 +0500

    test app

--- app/app.py
+++ app/app.py
@@ -1,43 +1,73 @@
 import re
-import sys
 import uuid
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from pathlib import Path
-
 from loguru import logger
+from pathlib import Path
+import socket
 
 logger.add('logs/app.log', format="[{time:YYYY-MM-DD HH:mm:ss}] | {level} | {message}")
 
-# import logger
+# import logger - РАБОТАЕТ!!!!!
 MAX_FILE_SIZE = 10 * 1024 * 1024  # 10 MB
-SERVER_ADDR = ('localhost', 8000)
+IMAGE_EXTENSION = ['.jpg', '.jpeg', '.png', '.gif']
 
-#Функция для генерации HTML страницы каталога
-def generate_gallery_page(image_files, base_url):
-    html = '<!DOCTYPE html>\n<html>\n<head>\n<title>Image Gallery</title>\n</head>\n<body>\n'
+SERVER_ADDR = ('0.0.0.0', 8000)
+
+
+# Функция генерации HTML после успешной загрузки картинки
+def generate_upload_success_page(image_id, ext):
+    html = ('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>Успешная загрузка</title>\n</head>\n'
+            '<body class="d-flex min-vh-100 justify-content-center align-items-center bg-light">\n')
+    html += '<div class="content-box bg-white col-12 col-md-8 col-lg-6 p-4 rounded-3 shadow">'
+    html += '<h1>Файл успешно загружен</h1>\n'
+    html += '</div>'
+    # Миниатюра изображения
+    html += f'<img src="/images/{image_id}.{ext}" alt="Загруженная картинка" style="max-width: 300px; max-height: 300px;">\n'
+
+    # Ссылка на скачивание
+    html += f'<p><a href="/images/{image_id}.{ext}" download>Скачать</a></p>\n'
+
+    # Ссылка на галерею
+    html += '<p><a href="/images">Каталог</a></p>\n'
+
+    html += '</body>\n</html>'
+    return html
+
+
+# Функция для генерации HTML страницы каталога
+def generate_gallery_page(image_files):
+    html = '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>Image Gallery</title>\n</head>\n<body>\n'
     html += '<h1>Uploaded Images</h1>\n'
+    html += '<a href="/" style="text-decoration: none;">\n'
+    html += '  <button><p>Главная</p></button>\n'
+    html += '</a>\n'
+
     html += '<div style="display: flex; flex-wrap: wrap;">\n'
 
     for filename in image_files:
         html += f'<div style="margin: 10px; text-align: center;">\n'
-        html += f'  <a href="{base_url}/{filename}" target="_blank">\n'
-        html += f'    <img src="{base_url}/{filename}" alt="{filename}" style="max-width: 200px; max-height: 200px;">\n'
+        html += f'  <a href="/images/{filename}" target="_blank">\n'  # Используем относительный путь
+        html += f'    <img src="/images/{filename}" alt="{filename}" style="max-width: 200px; max-height: 200px;">\n'
         html += f'  </a>\n'
         html += f'  <p>{filename}</p>\n'
+        html += f'<p><a href="/images/{filename}" download>Скачать</a></p>\n'
         html += '</div>\n'
 
     html += '</div>\n</body>\n</html>'
     return html
-#Функция для формирования списка файлов
+
+
+# Функция для формирования списка файлов
 def get_image_files(directory):
-    image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    image_extensions = ''
     image_files = []
 
     for file_path in Path(directory).iterdir():
-        if file_path.is_file() and file_path.suffix.lower() in image_extensions:
+        if file_path.is_file() and file_path.suffix.lower() in IMAGE_EXTENSION:
             image_files.append(file_path.name)
 
-    return image_files
+    return image_files, image_extensions
+
 
 # Парсер для распаковки multipart/form-data данных отправленных с формы
 def parse_multipart_form_data(headers, rfile, content_length):
@@ -75,92 +105,113 @@ def parse_multipart_form_data(headers, rfile, content_length):
 class ImageHostingHandler(BaseHTTPRequestHandler):
     server_version = 'ImageHosting'
 
+
+    routes_GET = {
+        '/images': 'get_images',
+        '/upload': 'get_upload',
+    }
+
+    routes_POST = {
+        '/upload': 'post_upload',
+    }
+
     def do_GET(self):
-        if self.path == '/images':
-            directory = 'images'
-            base_url = f'http://{SERVER_ADDR[0]}:{SERVER_ADDR[1]}/images'
-            image_files = get_image_files(directory)
-
-            html_content = generate_gallery_page(image_files, base_url)
-
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            self.wfile.write(html_content.encode('utf-8'))
-            return
-
-        # if self.path in ('/', '/index.html'):
-        #     logger.info(f'GET {self.path}')
-        #     self.send_response(200)
-        #     self.send_header('Content-type', 'text/html; charset=utf-8')
-        #     self.end_headers()
-        #     self.wfile.write(open('index.html', 'rb').read())
-        # else:
-        #     logger.warning(f'GET {self.path}')
-        #     self.send_response(400)
+        logger.info(f'GET {self.path}')
+        if self.path in self.routes_GET:
+            exec(f'self.{self.routes_GET[self.path]}()')
+        else:
+            logger.warning(f'GET 404 {self.path}')
+            self.send_response(404, 'Not found')
 
     def do_POST(self):
-        if self.path == '/upload':
-            logger.info(f'POST {self.path}')
-            content_length = int(self.headers.get('Content-Length', 0))
+        logger.info(f'POST {self.path}')
+        if self.path in self.routes_POST:
+            exec(f'self.{self.routes_POST[self.path]}()')
+        else:
+            self.send_response(404)
+            logger.warning(f'POST 404 {self.path}')
+            self.end_headers()
+            self.wfile.write(b'Not Found')
 
-            if content_length == 0:
-                logger.warning("Missing Content-Length header")
-                self.send_response(411)  # Length Required
+    def get_images(self):
+        directory = 'images'
+        image_files, image_extension = get_image_files(directory)
+
+        html_content = generate_gallery_page(image_files)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(html_content.encode('utf-8'))
+        return
+
+    def get_upload(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(open('upload.html', 'rb').read())
+        return
+
+    def post_upload(self):
+        logger.info(f'POST {self.path}')
+        content_length = int(self.headers.get('Content-Length', 0))
+
+        if content_length == 0:
+            logger.warning("Missing Content-Length header")
+            self.send_response(411)  # Length Required
+            self.end_headers()
+            return
+
+        # Проверяем Content-Type, через форму должен прйти multipart/form-data
+        content_type = self.headers.get('Content-Type', '')
+
+        if 'multipart/form-data' in content_type:
+            try:
+                filename, file_content = parse_multipart_form_data(self.headers, self.rfile, content_length)
+            except ValueError as e:
+                logger.error(f"Error parsing multipart/form-data: {e}")
+                self.send_response(400)  # Bad Request
                 self.end_headers()
                 return
-
-            #Проверяем Content-Type, через форму должен прйти multipart/form-data
-            content_type = self.headers.get('Content-Type', '')
-
-            if 'multipart/form-data' in content_type:
-
-                try:
-                    filename, file_content = parse_multipart_form_data(self.headers, self.rfile, content_length)
-                except ValueError as e:
-                    logger.error(f"Error parsing multipart/form-data: {e}")
-                    self.send_response(400)  # Bad Request
-                    self.end_headers()
-                    return
-            else:
-                # Прямое чтение бинарных данных
-                file_content = self.rfile.read(content_length)
-                filename = None
-
-            image_id = uuid.uuid4()
-
-
-            with open(f'app/images/{image_id}.jpg', 'wb') as f:
-                f.write(file_content)
-
-            logger.info(f'Upload succes {self.path}')
-            self.send_response(201)
-            self.send_header('Location', f'http://{SERVER_ADDR[0]}:{SERVER_ADDR[1]}/images/{image_id}.jpg')
-            self.end_headers()
-
         else:
-            self.send_response(405, 'Method not Allowed')
+            # Прямое чтение бинарных данных
+            file_content = self.rfile.read(content_length)
+            filename = None
 
+        extension = filename.split('.')[-1]
 
+        if f".{extension}" not in IMAGE_EXTENSION:
+            logger.error(f"Ошибка: неподдерживаемый формат файла .{extension}.")
+            self.send_response(400)  # Bad Request
+            self.end_headers()
+            return
 
+        image_id = uuid.uuid4()
 
+        with open(f'images/{image_id}.{extension}', 'wb') as f:
+            f.write(file_content)
 
+        logger.info(f'Успех: Изображение {image_id}.{extension} загружено')
+        # Генерируем HTML-страницу с миниатюрой и ссылками
+        html_content = generate_upload_success_page(image_id, extension)
 
-
+        # Отправляем HTML-страницу в ответ
+        self.send_response(200)  # OK
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(html_content.encode('utf-8')))
+        self.end_headers()
+        self.wfile.write(html_content.encode('utf-8'))
 
 
 def run():
-    server_address = ('localhost', 8000)
+    server_address = ('', 8000)
     httpd = HTTPServer(server_address, ImageHostingHandler)
     try:
-        print(f'Serving at http://{server_address[0]}:{server_address[1]}')
         httpd.serve_forever()
     except Exception:
         pass
     finally:
         httpd.server_close()
-
-
 
 
 if __name__ == '__main__':
